3acfee734ad2456ef1681f9fedcf97e5b0c1f445
amitroth <amitroth@gmail.com>
2023-05-02 19:04:45 +0300

    sbatch

--- sbatch.sh
+++ sbatch.sh
@@ -6,7 +6,7 @@
 #SBATCH --mem=64g
 module load cuda/11.3
 module load cudnn
-source /cs/labs/adiyoss/amitroth/FastSpeech/venv3.8.10/bin/activate.csh
+conda activate venv3.8.10
 
 # pip uninstall -y nvidia_cublas_cu11
 
