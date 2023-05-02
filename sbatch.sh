611781824e2fa3fbe43ce891320ca3b8b2f6e2b2
amitroth <amitroth@gmail.com>
2023-05-03 00:39:30 +0300

    sbatch

--- sbatch.sh
+++ sbatch.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-#SBATCH --ntasks-per-node=1
-#SBATCH --ntasks=1
-#SBATCH --gres=gpu:1,vmem:15g
-#SBATCH --cpus-per-task=40
-#SBATCH --mem=64g
-module load cuda/11.3
+#SBATCH --job-name=FastSpeech2-Train
+#SBATCH --gres=gpu:sm-60:1,vmem:15g
+#SBATCH -c20
+#SBATCH --mem=20g
+module load cuda/11.0
 module load cudnn
 conda activate venv3.8.10
 
 # pip uninstall -y nvidia_cublas_cu11
 
+# 1 WILL WORK
 export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
+setenv PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION python
 
 python3 train.py -p config/LJSpeech/preprocess.yaml -m config/LJSpeech/model.yaml -t config/LJSpeech/train.yaml
