7813461a955fc423b97ab0f43465ee47dc7288aa
amitroth <amitroth@gmail.com>
2023-05-07 14:11:21 +0300

    synth

--- synthesize.py
+++ synthesize.py
@@ -8,7 +8,7 @@ import numpy as np
 from torch.utils.data import DataLoader
 print("trying to import")
 import nltk
-nltk.download('corpora/cmudict.zip')
+nltk.download('corpora/cmudict')
 from g2p_en import G2p
 print("Suceeded")
 from pypinyin import pinyin, Style
